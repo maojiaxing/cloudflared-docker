668f48941e017ff77f0f899890a65a5780bd92fd
maojiaxing <maojiaxing.s@gmail.com>
2025-09-22 20:53:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,6 @@ if ! id "$USER" >/dev/null 2>&1; then
     echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/_sshd.conf
 fi
 
+envsubst '${PORT}' < /etc/ssh/config.template > /etc/ssh/sshd_config
+chmod 600 /etc/ssh/sshd_config
+chown root:root /etc/ssh/sshd_config
