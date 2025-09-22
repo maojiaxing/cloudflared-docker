065c8e3c91856a14c112441b7b4ba4f3ba45ccdb
maojiaxing <maojiaxing.s@gmail.com>
2025-09-22 20:49:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
+if ! id "$USER" >/dev/null 2>&1; then
+    echo "Creating user: $USER"
+    adduser -D "$USER"
+    echo "$USER:$PASSWORD" | chpasswd
+    # 添加sudo权限
+    echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+    echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/_sshd.conf
+fi
 
