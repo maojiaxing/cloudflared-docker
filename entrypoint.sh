b0842af7b4c21efb055c2d81a54999a225a07ff4
maojiaxing <maojiaxing.s@gmail.com>
2025-09-24 23:21:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ fi
 
 CONFIG_DIR="/home/$USER/.config"
 SUPERVISORD_CONF_DIR="$CONFIG_DIR/supervisor"
+CREDENTIALS_DIR="/home/$USER/.config/cloudflared"
+CREDENTIALS_FILE="$CREDENTIALS_DIR/credentials.json"
 
 if [ ! -d "$CONFIG_DIR" ]; then
   mkdir -p "$CONFIG_DIR"
@@ -21,8 +23,25 @@ if [ ! -d "$CONFIG_DIR" ]; then
   mkdir -p "$SUPERVISORD_CONF_DIR/conf.d"
 fi
 
+if [ -z "$TUNNEL_TOKEN" ]; then
+    echo "错误：未提供TUNNEL_TOKEN环境变量"
+    exit 1
+fi
+
+if [ ! -d "$CREDENTIALS_DIR" ]; then
+    mkdir -p "$CREDENTIALS_DIR"
+    chown "$USER:$USER" "$CREDENTIALS_DIR"
+    chmod 700 "$CREDENTIALS_DIR"
+fi
+
+# 生成凭证文件
+echo "{\"TunnelToken\":\"$TUNNEL_TOKEN\"}" > "$CREDENTIALS_FILE"
+chown "$USER:$USER" "$CREDENTIALS_FILE"
+chmod 600 "$CREDENTIALS_FILE"
+
 cp /tmp/supervisord.conf "$SUPERVISORD_CONF_DIR/"
-envsubst '$USER' < /tmp/sshd.service > "$SUPERVISORD_CONF_DIR/conf.d/sshd.service"
-envsubst '$USER' < /tmp/cloudflare.service > "$SUPERVISORD_CONF_DIR/conf.d/cloudflare.service"
+envsubst '$USER $PORT' < /tmp/sshd.service > "$SUPERVISORD_CONF_DIR/conf.d/sshd.service"
+envsubst '$USER' < /tmp/cloudflared.service > "$SUPERVISORD_CONF_DIR/conf.d/cloudflared.service"
+chmod 755 /var/log/supervisor
 
 exec supervisord -c /home/nonroot/.config/supervisor/supervisord.conf
