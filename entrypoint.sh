217203fa91395cfe566ca4a51148143ca987226d
maojiaxing <maojiaxing.s@gmail.com>
2025-09-24 22:57:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,22 @@ if ! id "$USER" >/dev/null 2>&1; then
     echo "$USER:$PASSWORD" | chpasswd
     # 添加sudo权限
     echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-    echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/_sshd.conf
+    echo 'Port $PORT' > /etc/ssh/sshd_config
+    echo 'PermitRootLogin no' >> /etc/ssh/sshd_config
+    chmod 600 /etc/ssh/sshd_config
+    chown root:root /etc/ssh/sshd_config
 fi
 
-envsubst '${PORT}' < /etc/ssh/config.template > /etc/ssh/sshd_config
-chmod 600 /etc/ssh/sshd_config
-chown root:root /etc/ssh/sshd_config
+CONFIG_DIR="/home/$USER/.config"
+SUPERVISORD_CONF_DIR="$CONFIG_DIR/supervisor"
 
-tunnel --no-autoupdate run --token $TOKEN
+if [ ! -d "$CONFIG_DIR" ]; then
+  mkdir -p "$CONFIG_DIR"
+  mkdir -p "$SUPERVISORD_CONF_DIR"
+  mkdir -p "$SUPERVISORD_CONF_DIR/conf.d"
+fi
+
+cp /tmp/supervisord.conf "$SUPERVISORD_CONF_DIR/"
+envsubst '$USER' < /tmp/sshd.service > "$SUPERVISORD_CONF_DIR/conf.d/sshd.service"
+envsubst '$USER' < /tmp/cloudflare.service > "$SUPERVISORD_CONF_DIR/conf.d/cloudflare.service"
+
